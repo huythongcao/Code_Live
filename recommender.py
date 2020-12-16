e62285566388e260cfda33d2e6559f49d26f32ac
huythongcao <1801040214@s.hanu.edu.vn>
2020-12-16 10:41:07 +0700

    first commit

---
+++ recommender.py
@@ -0,0 +1,12 @@
+
+def introduction():
+    print("Welcome to the CSC110 Book Recommender. Type the word in the left column to do the action on the right.")
+    print("recommend : books for a particular user")
+    print("averages  : output the average ratings of all books in the system")
+    print("quit      : exit the program")
+    print("next task?")
+
+
+if __name__ == '__main__':
+
+introduction()
